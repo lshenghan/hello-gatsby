59071cc1dad98b19210f7263695f8fd0e031ac01
lsheng <lsheng.han@gmail.com>
2021-06-03 10:34:40 +0800

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
-import * as React from "react"
+import * as React from 'react'
 import Layout from '../components/layout'
-
-// styles
-
-
-// data
-
-// markup
+import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="https://img1.nickiswift.com/img/gallery/talk-show-hosts-reveal-their-worst-guests-ever/intro-1571328001.jpg"
+      />
     </Layout>
   )
 }
